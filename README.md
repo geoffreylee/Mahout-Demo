5b154bb9d4e4432bc92c9bcf747da73201ae4bb0
Geoffrey <geoffrey.y.lee@gmail.com>
2014-08-28 13:05:48 -0700

    modified readme, added php script to call server app

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
-Mahout-Demo
-===========
+Mahout-Demo by Geoffrey Lee (c) Tamber, Inc. 2014
+=================================================
 
-Open sourced implementation of Mahout for comparison test against Engie (TM)
+This is the code which I developed in order to build a basic impelmentation of an Apache Mahout User Based Recommender. The recommender is compared against Tamber's proprietary recommendation engine named Engie with regards to speed and accuracy. The comparison test is currently hosted at http://engie.org/index_new.html although there are plans to move this page to http://engie.org
+
+Right now, Mahout is being trained on the Movie Lens database; the file that I have included is the 100k ratings file. The file which the demo hosted at engie.org is trained on is the 10M ratings file (if you want Mahout to parse the 10M on a medium instance you have to give the VM more heap space via command line).
+
+You will notice that a user preference of 4.0 is hard-coded in for each movie ID submitted to the recommender; this is because this is the same information which we provide to Engie (for ease of user experience, we merely have users select movies rather than rate them out of 5 although this could easily be changed with a few lines of code; since both Engines are trained on the ML database which rates out of 5, this was our approximation for how to do a comparison). I may later upload a more flexible version that does not hard code in the user preference.  
+
+If you want to run the local app, pass via command line a list of movie IDs separated by space. If you wish to run the server app, you must open port 24579 and execute the server app as a background service. You can pass it commands by writing to a socket. I have also uploaded a basic php script demonstrating this. 
---
+++ call_mahout.php
@@ -0,0 +1,67 @@
+<?php
+/* A basic PHP wrapper of the Mahout API 
+ * returns results in JSON format
+ * by Geoffrey Lee (c) Tamber, Inc. 2014
+ */
+
+/* USAGE: pass the script the field likes with ID's separated by \x1f;
+ * For example: http://yourdomain.com/call_mahout.php?likes=100\x1f25
+ */
+
+// This is the IP address at which the Mahout server app is hosted. 
+$MAHOUTIP = "127.0.0.1";
+try {
+	header("Cache-control: no-cache");
+	header("Content-type: application/json; charset=utf-8");
+	$time = microtime(true);
+	if (isset($_GET["likes"])) {
+		$result = parse_mahout($_GET['likes']);
+		$result["time"] = microtime(true) - $time;
+		echo json_encode($result);
+	} else {
+		echo json_encode(array("success" => false, "time" => microtime(true) - $time, "error" => "no likes given"));
+	}
+} catch (Exception $e) {
+	echo json_encode(array("success" => false, "time" => microtime(true) - $time, "error" => "unexpected error " . $e->getMessage()));
+}
+
+function parse_mahout($likes) {
+	$likearg = str_replace("\\x1f", " ", $likes);
+	if(trim($likearg) == "") {
+		return array("success"=>false, "error" => "please select some movies");
+	}
+	$r = call_backend('rec', $likearg);
+	$sugg = array();
+	$rp = explode(' ', $r);
+	foreach($rp as $suggested) {
+		$suggested = trim($suggested);
+		if($suggested!= "") {
+			$suggv = explode(":", $suggested);
+			$item = array("movie_key" => $suggv[0], "rating" => $suggv[1]);
+			array_push($sugg, $item);
+		}	
+	}
+	return array("success" => "true", "result" => $sugg);
+}
+
+function call_backend($command, $arg) {
+	$port = 24579;
+	$conn = fsockopen($MAHOUTIP, $port);
+	if ($conn === FALSE) {
+		$conn = fsockopen($MAHOUTIP, $port);
+		if ($conn === FALSE) {
+			echo "Mahout Backend Server Not Found";
+		}
+	}
+	fwrite($conn, $command . " " . $arg . "\n");
+	
+	$result = fgets($conn);
+	while(!feof($conn)) {
+		$result = $result . fgets($conn);
+		if ($conn === FALSE) {
+			break;
+		}
+	}
+	return $result;
+}
+?>
