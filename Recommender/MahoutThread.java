b2890f4c50e607fbfded7a328a47ed07d63d0a09
Geoffrey <geoffrey.y.lee@gmail.com>
2015-03-25 12:43:58 -0700

    fixed multithreading; put the listener in the multithreader and opted not to use executor to not have to limit threading

--- Recommender/MahoutThread.java
+++ Recommender/MahoutThread.java
@@ -1,42 +1,63 @@
 package Recommender;
-import java.util.concurrent.Callable;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.ArrayList;
 import java.util.List;
-
 import org.apache.mahout.cf.taste.common.TasteException;
-
 import Recommender.ItemRecommend;
-
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 
-public class MahoutThread implements Callable<List<RecommendedItem>> {
+public class MahoutThread implements Runnable {
 	private static final Logger logger = Logger.getLogger(App.class.getName());
-	protected String threadName;
+	protected Socket socket;
 	protected Thread t;
 	protected ItemRecommend Mahout;
-	protected Long uid;
-	protected Integer n; 
-	protected List<RecommendedItem> recs;
 
 	// Making the mahout thread is essentially going to be the data of reading a rec
-	MahoutThread(ItemRecommend recommender, Long userID, Integer num) {
+	MahoutThread(Socket s, ItemRecommend recommender) {
 		// we dont want to instantiate multiple copies of mahout so we pass it to the thread
-		Mahout = recommender;
-		uid = userID;
-		n = num;
+		this.Mahout = recommender;
+		this.socket = s;
 	}
-	
 	@Override
-	// The main computation
-	public List<RecommendedItem> call() {
+	public void run() {
 		try {
-			return Mahout.getRecommendations(uid, n);
+			PrintWriter pw = new PrintWriter(this.socket.getOutputStream(), true);
+			BufferedReader br = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+			String cmd = br.readLine();				
+			String[] heard = cmd.split("\\s");
+			
+			if(heard[0].equals("uid")) {
+				Long uid = Long.parseLong(heard[1]);		
+				List<RecommendedItem> recommendations = this.Mahout.getRecommendations(uid, 30);
+				
+				// read off the recs and parse
+				String recs = "";
+				for(RecommendedItem recommendation: recommendations) {
+					long itemID = recommendation.getItemID();
+					recs = recs + itemID + ":" + recommendation.getValue() + " ";
+				}
+				recs = recs + "\n";
+				pw.println(recs);
+			} 
+			pw.close();
+			socket.close();
+			
+		} catch (IOException e) {
+			logger.log(Level.SEVERE, e.toString());
 		} catch (TasteException e) {
 			logger.log(Level.SEVERE, e.toString());
-			return new ArrayList<RecommendedItem>();
 		}
 	}
 	
+	public void start() {
+		if (this.t == null) {
+			t = new Thread(this, "recommender-thread");
+			t.start();
+		}
+	}
 }
--- Recommender/MultithreadedApp.java
+++ Recommender/MultithreadedApp.java
@@ -1,17 +1,8 @@
 package Recommender;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
-import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 
 import Recommender.ItemRecommend; 
 import Recommender.MahoutThread;
@@ -19,52 +10,22 @@ import Recommender.MahoutThread;
 public class MultithreadedApp {
 	private static final Logger logger = Logger.getLogger(App.class.getName());
 	public static void main(String[] args) throws Exception {
-		
 		// Where Mahout will listen
 		final int portNumber = 24579;
 		ServerSocket serverSocket = new ServerSocket(portNumber);
 		// Initialize; right now the format is for a csv training file though this could easily be made into a db
 		ItemRecommend Mahout = new ItemRecommend("data/users.data.csv", 100, 100, "loglikelihood");
-		// Multithreading to support 8 cores
-		ExecutorService executor = Executors.newFixedThreadPool(8);
-		
+			
 		// Listen
 		while(true) {	
+			logger.log(Level.INFO, "Mahout listening");
 			try {
 				Socket socket = serverSocket.accept();
-				PrintWriter pw = new PrintWriter(socket.getOutputStream(), true);
-				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				String cmd = br.readLine();				
-				String[] heard = cmd.split("\\s");
-				
-				if(heard[0].equals("uid")) {
-					Long uid = Long.parseLong(heard[1]);
-					
-					// async submit task
-					Future<List<RecommendedItem>> task = executor.submit(new MahoutThread(Mahout, uid, 30));
-					List<RecommendedItem> recommendations = task.get();
-					
-					// read off the recs and parse
-					String recs = "";
-					for(RecommendedItem recommendation: recommendations) {
-						long itemID = recommendation.getItemID();
-						recs = recs + itemID + ":" + recommendation.getValue() + " ";
-					}
-					recs = recs + "\n";
-					pw.println(recs);
-				} else if(heard[0].equals("quit")) {
-					//cleanup 
-					pw.close();
-					socket.close();
-					serverSocket.close();
-					System.exit(0);
-				}
-				pw.close();
-				socket.close();
-			} catch (NullPointerException e) {
-				logger.log(Level.SEVERE, e.toString());
+				// start a new thread to handle everything
+				new Thread(new MahoutThread(socket, Mahout)).start();
+			} finally {
+				serverSocket.close();
 			}
-		} 
+		}
 	}
-
 }
